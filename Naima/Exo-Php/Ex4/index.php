f01a89d00ad0e49e34f5c56493d16a6dc801497d
Naiima973 <Naiima.C@hotmail.fr>
2019-12-02 15:16:24 -0300

    Finish

---
+++ Naima/Exo-Php/Ex4/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Exercice4</title>
+</head>
+<body>
+    <?php
+
+$string = 'Chaine';
+$integer= (int)1995;
+$float  = 2.4;
+$boolean = true;
+
+echo $string;
+echo '<br/>';
+
+echo $integer;
+echo'<br/>';
+
+echo $float;
+echo '<br/>';
+
+echo $boolean;
+echo '<br/>';
+
+    ?>
+</body>
+</html>
