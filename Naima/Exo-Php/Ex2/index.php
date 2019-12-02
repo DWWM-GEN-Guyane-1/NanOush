640c8a057b57b258387c26441416caac1cb65540
Naiima973 <Naiima.C@hotmail.fr>
2019-12-02 14:11:51 -0300

    Finish

---
+++ Naima/Exo-Php/Ex2/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Exercice2</title>
+</head>
+<body>
+<?php
+  $nom = 'CIMPER';
+  $prenom = 'Naima';
+  $age = 23;
+ 
+  echo $nom;         // Affiche 'CIMPER'
+  echo '<br/>';
+  echo $prenom;        // Affiche 'Naima'
+  echo '<br/>';
+  echo $age;        // Affiche 23
+ 
+ 
+ 
+?>
+</body>
+</html>
