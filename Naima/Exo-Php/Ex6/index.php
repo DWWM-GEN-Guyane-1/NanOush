be7138c32aa43b920ecad92129fc9677c98e8872
Naiima973 <Naiima.C@hotmail.fr>
2019-12-02 19:18:14 -0300

    Finish

---
+++ Naima/Exo-Php/Ex6/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Exercice6</title>
+</head>
+<body>
+    <?php
+ 
+ $nom = 'CIMPER';
+ $identite = '';
+ 
+ $identite = 'Bonjour' . $nom . 'Comment vas-tu?';
+ 
+ echo $identite;
+    ?>
+</body>
+</html>
