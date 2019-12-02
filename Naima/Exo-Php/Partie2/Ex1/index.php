a713f2e6c5f51db0010841d21bfdf2472ed96c79
Naiima973 <Naiima.C@hotmail.fr>
2019-12-02 19:53:56 -0300

    Finish

---
+++ Naima/Exo-Php/Partie2/Ex1/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Exercice1</title>
+</head>
+<body>
+    <?php
+$age = 12;
+
+if ($age < 18)
+{echo 'Vous êtes mineur';}
+else
+{echo 'Vous êtes majeur';}
+    ?>
+</body>
+</html>
