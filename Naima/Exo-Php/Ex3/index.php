7f13a833a296679c628dde8c66aabb6b1b1559b7
Naiima973 <Naiima.C@hotmail.fr>
2019-12-02 14:40:49 -0300

    Finish

---
+++ Naima/Exo-Php/Ex3/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Exercice3</title>
+</head>
+<body>
+<?php
+
+$km = '1';
+ echo $km;
+ echo '<br/>';
+
+$km ='3';
+ echo $km;
+ echo '<br/>';
+
+ $km ='125';
+ echo $km;
+ echo '<br/>';
+?>
+</body>
+</html>
