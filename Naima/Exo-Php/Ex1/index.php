f6aadfb37d3a5bb1b3b1de94a8b8464c08e8fe1e
Naiima973 <Naiima.C@hotmail.fr>
2019-12-02 11:51:38 -0300

    Add

---
+++ Naima/Exo-Php/Ex1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="Fr">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Exercice1</title>
+</head>
+<body>
+
+    <?php
+    
+    echo 'Bonjour'
+      $prenom = 'Naïma';  // Type string (chaine de caractères)
+      $nom = "CIMPER";    // Type string (chaine de caractères)
+    ?>
+</body>
+</html>
