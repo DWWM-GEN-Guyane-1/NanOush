4287d4accf396a4cdf25bac41bba65533f9bae79
Naiima973 <Naiima.C@hotmail.fr>
2019-12-02 15:31:22 -0300

    Finish

---
+++ Naima/Exo-Php/Ex5/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Exercice5</title>
+</head>
+<body>
+    <?php
+
+$int = null;
+echo $int;
+echo '<br/>';
+
+$int = 12;
+echo $int;
+echo '<br/>';
+
+    ?>
+</body>
+</html>
