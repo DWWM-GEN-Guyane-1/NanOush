d4cc50c5075fb9b45208eac7805ca484630fd485
Naiima973 <Naiima.C@hotmail.fr>
2019-12-02 19:24:43 -0300

    Finish

---
+++ Naima/Exo-Php/Ex7/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Exercice7</title>
+</head>
+<body>
+    <?php
+
+$nom = 'CIMPER';
+
+$prenom = 'Naima';
+
+$int= 23;
+
+$identite = '';
+
+$identite = 'Bonjour' . $nom . $prenom . 'tu as' . $int . 'ans';
+
+echo $identite;
+?>
+</body>
+</html>
